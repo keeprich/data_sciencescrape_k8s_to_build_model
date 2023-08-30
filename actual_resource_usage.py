0051f21c9dc4da8a6fc42bc4aa382149fb8c036f
keeprich <35325509+keeprich@users.noreply.github.com>
2023-08-30 10:51:16 +0100

    Update actual_resource_usage.py

--- actual_resource_usage.py
+++ actual_resource_usage.py
@@ -1,15 +1,14 @@
 from kubernetes import client, config
-import datetime
 
 # Load Kubernetes configuration (you should have a valid kubeconfig)
 config.load_kube_config()
 
 # Create a Kubernetes API client for Pods and Metrics
 api = client.CoreV1Api()
-metrics_api = client.MetricsV1Api()
+metrics_api = client.CustomObjectsApi()
 
 # Define the namespace
-namespace = "default"
+namespace = "your-namespace"
 
 # List all pods in the namespace
 try:
@@ -18,23 +17,19 @@ except Exception as e:
     print(f"Error listing pods in namespace {namespace}: {e}")
     exit(1)
 
-# Get current time for the timestamp
-current_time = datetime.datetime.now()
-
 # Display resource usage for each pod
 for pod in pod_list.items:
     pod_name = pod.metadata.name
 
     # Fetch resource metrics for the pod
     try:
-        metrics = metrics_api.list_namespaced_pod(namespace=namespace, metric_selector=f"pod=={pod_name}")
-        for metric in metrics.items:
-            container_name = metric.metadata.name
-            cpu_usage = metric.containers[0].usage["cpu"]
-            memory_usage = metric.containers[0].usage["memory"]
+        metrics = metrics_api.get_namespaced_pod_metrics(name=pod_name, namespace=namespace)
+        for container_metric in metrics.containers:
+            container_name = container_metric.name
+            cpu_usage = container_metric.usage["cpu"]
+            memory_usage = container_metric.usage["memory"]
 
             print(f"Pod: {pod_name}")
-            print(f"Timestamp: {current_time}")
             print(f"Container: {container_name}")
             print(f"CPU Usage: {cpu_usage}")
             print(f"Memory Usage: {memory_usage}")
